7c446eb6e8095e7f3057c8d5ca6bf35cc15c2dfa
Raymond Chen <ahrchen@gmail.com>
2022-04-05 12:25:16 -0700

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Raymond Chen on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
